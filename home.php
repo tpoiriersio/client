881af2e228597e8cdcd1beaf85fef3192d765aab
tpoiriersio <poiriertheoo@gmail.com>
2021-04-17 18:37:26 +0200

    push update + delete, attente de token

--- home.php
+++ home.php
@@ -6,6 +6,10 @@
     $loader = new FilesystemLoader(__DIR__ . '/templates');
     $twig = new Environment($loader);
 
+    // On récupère la liste des ressources vérifiées
+    //$getResourcesIsVerified = "http://localhost:5000/ressources/isVerified";
+    //$resourcesList = json_decode(file_get_contents($getResourcesIsVerified), true);
+
     echo $twig->render('home.tpl', [
         'titre_page' => 'Home',
         'prenom' => 'John',
